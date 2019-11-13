a2774784531f94f9ec72c1d7e235567b4999c8fe
Dusan <dodalovic@gmail.com>
2019-11-13 14:42:02 +0100

    Add dummy cart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./views/Home";
 import Category from "./views/Category";
 import Product from "./views/Product";
+import Cart from "./views/Cart";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/categories/:slug" component={Category} />
         <Route path="/products/:slug" component={Product} />
+        <Route path="/cart" component={Cart} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/views/Cart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Cart() {
+    return (<div><h1>My Cart</h1></div>);
+}
+
+export default Cart;
